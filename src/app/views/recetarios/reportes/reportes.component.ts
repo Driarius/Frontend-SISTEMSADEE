1934b09d0e3afe93f35823fc2ee6089781b0e844
XIAC <x.i.a.c.1504@gmail.com>
2024-05-21 21:33:17 -0400

    CRUD RECETAS

--- src/app/views/recetarios/reportes/reportes.component.ts
+++ src/app/views/recetarios/reportes/reportes.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {RowComponent, ColComponent,CardComponent,  
-  CardHeaderComponent, CardBodyComponent} from '@coreui/angular';
+import {RowComponent, ColComponent,CardComponent,
+  CardHeaderComponent, CardBodyComponent,
+  ButtonDirective} from '@coreui/angular';
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [RowComponent, ColComponent,CardComponent,  
-    CardHeaderComponent, CardBodyComponent],
+  imports: [RowComponent, ColComponent,CardComponent,
+    CardHeaderComponent, CardBodyComponent, ButtonDirective],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.scss'
 })
